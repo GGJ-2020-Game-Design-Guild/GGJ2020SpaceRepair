b5134df0cd24f261b05d8b3e1ef8ca2f762e23b4
Connor Haynes <theconnorhaynes@gmail.com>
2020-02-01 13:35:24 -0500

    able to read dialogue from json

---
+++ Assets/ConnorWork/Dialogue shit/ReadInDialogue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadInDialogue : MonoBehaviour
+{
+    Dialogue[] dialogues;
+    public TextAsset json;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogues = JsonUtility.FromJson<Dialogue[]>(json.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
