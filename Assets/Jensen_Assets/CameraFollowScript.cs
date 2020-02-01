d91f126d16fa2f355029404883e07d0a066ba735
Tucker <tucker166@live.marshall.edu>
2020-02-01 12:21:33 -0500

    Camera Follow Script Added

---
+++ Assets/Jensen_Assets/CameraFollowScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+
+    public GameObject followObject;
+    public float lerpValue = 0.1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (followObject)
+        {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(followObject.transform.position.x, followObject.transform.position.y, transform.position.z), lerpValue);
+        }
+    }
+}
