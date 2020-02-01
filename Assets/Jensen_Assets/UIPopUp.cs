b0ca6ca3eb50d25b8ba0c3bf201582a3bd562b1b
Tucker <tucker166@live.marshall.edu>
2020-02-01 13:01:35 -0500

    Added UI Dialouge Box
    Awake on Trigger Enter, Code for Typing the Words

---
+++ Assets/Jensen_Assets/UIPopUp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPopUp : MonoBehaviour
+{
+    public GameObject playerCanvas;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "DioTrigger")
+        {
+            playerCanvas.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "DioTrigger")
+        {
+            playerCanvas.SetActive(false);
+        }
+    }
+}
