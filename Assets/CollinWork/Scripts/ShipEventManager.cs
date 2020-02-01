b6a2d8cf5f87bf1c08e8befc5887f91fbf1f7a9f
Tucker <tucker166@live.marshall.edu>
2020-02-01 16:07:59 -0500

    Random Ship Breaking and Event Timeline

--- Assets/CollinWork/Scripts/ShipEventManager.cs
+++ Assets/CollinWork/Scripts/ShipEventManager.cs
@@ -6,19 +6,30 @@ using UnityEngine;
 public class ShipEventManager : MonoBehaviour
 {
     // Lists of ship machinery, parts, and tools.
-    public List<GameObject> shipMachinery;
-    public List<GameObject> shipParts;
-    public List<GameObject> tools;
+    public List<string> shipMachinery;
+    public List<string> shipParts;
+    public List<string> tools;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shipMachinery.Add("shipMachine1");
+        shipMachinery.Add("shipMachine2");
+        shipMachinery.Add("shipMachine3");
+
+        shipParts.Add("shipPart1");
+        shipParts.Add("shipPart2");
+        shipParts.Add("shipPart3");
+
+        tools.Add("tool1");
+        tools.Add("tool2");
+        tools.Add("tool3");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void damageSelection()
     {
-        
+        Debug.Log(shipMachinery[Random.Range(0, shipMachinery.Count)]);
+        Debug.Log(shipParts[Random.Range(0, shipParts.Count)]);
+        Debug.Log(tools[Random.Range(0, tools.Count)]);
     }
 }
---
+++ Assets/Jensen_Assets/ShipTimeline.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipTimeline : MonoBehaviour
+{
+    ShipEventManager eventManagerScript;
+
+    void Start()
+    {
+        eventManagerScript = FindObjectOfType<ShipEventManager>();
+
+        Invoke("event1", 10.0f);
+        Invoke("event2", 20.0f);
+        Invoke("event3", 30.0f);
+    }
+
+    void event1()
+    {
+        eventManagerScript.damageSelection();
+    }
+
+    void event2()
+    {
+        eventManagerScript.damageSelection();
+
+    }
+
+    void event3()
+    {
+        eventManagerScript.damageSelection();
+    }
+
+}
