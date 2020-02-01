a3dfd9e2bededbe041fb20a1aa5ca7800d60159a
Connor Haynes <theconnorhaynes@gmail.com>
2020-02-01 12:28:52 -0500

    add beginning of interaction system

---
+++ Assets/ConnorWork/Item.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : Interactable
+{
+    public string name;
+    public Texture2D sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void interact(PlayerInventory pi) {
+        if (pi.item == null) {
+            pi.setItem(this);
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/ConnorWork/PlayerInventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public Item item;
+    Texture2D heldItemSprite;
+    string heldItemName;
+    public int facing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        facing = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0.5f)
+        {
+            facing = 1;
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            facing = 3;
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f)
+        {
+            facing = 0;
+        }
+
+        if (Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            facing = 2;
+        }
+
+        if (Input.GetButtonDown("")) {
+
+        }
+    }
+
+    public void setItem(Item i) {
+        item = i;
+        heldItemSprite = i.sprite;
+        heldItemName = i.name;
+    }
+}
