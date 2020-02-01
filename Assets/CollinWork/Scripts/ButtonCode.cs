8457df4bfb2e790c3b2c0247500cf50689ed780e
testmaster217 <55296006+testmaster217@users.noreply.github.com>
2020-02-01 13:17:16 -0500

    Added main menu.
    Added play button that starts the game, quit button that quits the game, and credits button that opens credits menu (which has a back button).

---
+++ Assets/CollinWork/Scripts/ButtonCode.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonCode : MonoBehaviour
+{
+    // Canvases for opening and closing the credits and the main menu.
+    public Canvas creditsCanvas;
+    public Canvas menuCanvas;
+
+    // Function that starts the game when you hit the play button.
+    public void OpenGame()
+    {
+        SceneManager.LoadSceneAsync("CollinTargetScene");
+    }
+
+    // Function that quits the game when you hit the quit button.
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    // Function that opens and closes the credits.
+    public void ToggleCredits()
+    {
+        // If the credits canvas isn't enabled, enable it and disable the main menu.
+        if (!creditsCanvas.gameObject.activeSelf)
+        {
+            creditsCanvas.gameObject.SetActive(true);
+            menuCanvas.gameObject.SetActive(false);
+        }
+        // Otherwise, disable the credits and enable the main menu.
+        else
+        {
+            creditsCanvas.gameObject.SetActive(false);
+            menuCanvas.gameObject.SetActive(true);
+        }
+    }
+}
