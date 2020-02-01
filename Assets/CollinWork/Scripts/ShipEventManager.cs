3e485f45ad6fdbf90a1646a638ccb5aef3da488d
testmaster217 <55296006+testmaster217@users.noreply.github.com>
2020-02-01 15:46:25 -0500

    Added ship event manager.
    Just has three empty public lists at the moment for machinery, parts, and tools.

---
+++ Assets/CollinWork/Scripts/ShipEventManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ShipEventManager : MonoBehaviour
+{
+    // Lists of ship machinery, parts, and tools.
+    public List<GameObject> shipMachinery;
+    public List<GameObject> shipParts;
+    public List<GameObject> tools;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/CollinWork/Scripts/ShipPart_v2.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipPart_v2 : Interactable
+{
+    const float MAX_HEALTH = 100;
+    public float health;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = MAX_HEALTH;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override public void interact(PlayerInventory pi) {
+        health = MAX_HEALTH;
+    }
+
+    public void damage() {
+        health -= 10;
+        if (health <= 0) {
+            //something bad
+        }
+    }
+}
