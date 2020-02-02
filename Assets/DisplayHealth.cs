50519c1dd6d59cced95d31b504c4b3b35ec94166
Connor Haynes <theconnorhaynes@gmail.com>
2020-02-02 11:58:53 -0500

    maybe gradient?

--- Assets/DisplayHealth.cs
+++ Assets/DisplayHealth.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class DisplayHealth : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Color color;
 
+    public Color green;
+    public Color yellow;
+    public Color red;
     public float health;
 
     void Start()
     {
-        
+        color = gameObject.GetComponent<SpriteRenderer>().color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.GetComponent<SpriteRenderer>().color
+        gradientUpdate();
+    }
+
+    public void gradientUpdate() {
+        Color color1;
+        Color color2;
+        if (health > 50)
+        {
+            color1 = green;
+            color2 = yellow;
+        }
+        else {
+            color1 = yellow;
+            color2 = red;
+        }
+
+        color.r = color1.r + (color1.r - color2.r) * (health % 50) / 50.0f;
+        color.g = color1.g + (color1.g - color2.g) * (health % 50) / 50.0f;
+        color.b = color1.b + (color1.b - color2.b) * (health % 50) / 50.0f;
     }
 }
