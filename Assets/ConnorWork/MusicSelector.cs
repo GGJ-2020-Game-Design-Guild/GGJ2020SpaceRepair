ac3e709ee43e9769f3b5c436e352639c153a02c5
Connor Haynes <theconnorhaynes@gmail.com>
2020-02-02 14:50:28 -0500

    add music player

---
+++ Assets/ConnorWork/MusicSelector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSelector : MonoBehaviour
+{
+    public AudioClip[] music;
+    private AudioSource source;
+    int track;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        track = (int)Mathf.Round(Random.Range(0, music.Length));
+        source.clip = music[track];
+        source.Play();
+        source.loop = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (!source.isPlaying) {
+            if (track < music.Length)
+                track++;
+            else
+                track = 0;
+            source.clip = music[track];
+            source.Play();
+        }
+    }
+}
