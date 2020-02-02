05d05b5718c2b35ae254d6e6aeeb00c1933c1351
Izzy Bennett <buildgamesnotbombs@gmail.com>
2020-02-02 11:45:21 -0500

    Started Display here

---
+++ Assets/DisplayHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float health;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.gameObject.GetComponent<SpriteRenderer>().color
+    }
+}
