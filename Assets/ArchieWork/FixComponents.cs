f9c2309b7e44c0da033572d80b9cdf68d28b723d
Archie Petin <33238302+abpetin@users.noreply.github.com>
2020-02-02 17:23:30 -0500

    Update FixComponents.cs

--- Assets/ArchieWork/FixComponents.cs
+++ Assets/ArchieWork/FixComponents.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class FixComponents : MonoBehaviour
 {
+
+    public bool hasItem = false;
+    public GameObject requiredItem;
+    public GameObject requiredTool;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +18,15 @@ public class FixComponents : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        requiredItem = GetComponent<ShipPart>().requiredItem;
+        requiredTool = GetComponent<ShipPart>().requiredTool;
     }
 
     ///
+    void FixComponent()
+    {
         
+    }
+
     ///
 }
